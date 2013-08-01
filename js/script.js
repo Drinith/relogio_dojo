27b3e33b6803eea1bfd1ecd0fa479c7cff3c8af9
Drinith <felpmel@gmail.com>
2013-08-01 18:08:56 -0300

    Termino das funções do contador

--- js/script.js
+++ js/script.js
@@ -1,19 +1,66 @@
 
 $(function(){
 
-	var valorMin;
-	var valorSeg;
-	var valorTempo;
+	var valorMin=0;
+	var valorSeg=0;
+	var valorTempo=0;
+	var intervalo=0;
+	var min=0;
+	var seg=0;
+	var tempo=0;
+	
+	
 
-	//Contagem
-	var contagem = function(){
+	function contagem(){
+		// Se o tempo não for zerado
+		if((tempo) >= 0){
+			
+			// Pega a parte inteira dos minutos
+			var min = parseInt(tempo/60);
+			// Calcula os segundos restantes
+			var seg = tempo%60;					
 
-		valorTempo = parseInt(valorMin)*60+parseInt(valorSeg);
+			// Formata o número menor que dez, ex: 08, 07, ...
+			if(min < 10){
+				valorMin = "0"+min;
+			}
+			else{
+				valorMin=min;
+			}
+			
+			if(seg <=9){
+				valorSeg = "0"+seg;
+			}
+			else{
+				valorSeg = seg;
+			}
 
+				// Cria a variável para formatar no estilo hora/cronômetro
+				$('input[name="dplMin"]').val(valorMin);
+				$('input[name="dplSeg"]').val(valorSeg);
+				tempo--;
+		}
+		else{
+		clearInterval(intervalo);
+		}
+
+	}//fim contagem
+
+	
+	function setContagem (){
+
+		min = parseInt($('input[name="dplMin"]').val());
+		seg = parseInt($('input[name="dplSeg"]').val());
+		tempo = min*60+seg;
+
+
+		intervalo = setInterval(contagem,1000);
+
+			
 	}
 
 	//testar Campos
-	var testarCampos = function (){
+	function testarCampos(){
 		var testeCampo = true;
 		//pegando os valores dos campos
 		valorMin = $('input[name="dplMin"]').val();
@@ -39,7 +86,7 @@ $(function(){
 		
 	}//fim teste campo
 
-	var testeNum =function (num){
+	function testeNum(num){
 
 		var er = /^[0-9]+$/;		
 
@@ -62,7 +109,7 @@ $(function(){
 		//testar campo
 		if (testarCampos()){
 			//iniciar contagem
-			contagem();	
+			setContagem();	
 		}
 		
 
